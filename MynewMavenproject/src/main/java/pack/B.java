698af8bbede621b9689d0311d9f89b03711022e8
HP <HP@DESKTOP-68DH6QH>
2024-10-25 06:20:17 +0530

    Adding new files to repository

---
+++ MynewMavenproject/src/main/java/pack/B.java
@@ -0,0 +1,16 @@
+package pack;
+
+public class B extends A{
+    String college="CET";
+public void test() {
+System.out.println("Tests");
+}
+
+	public static void main(String[] args) {
+		
+			B b =new B();
+			System.out.println(b.a);
+			b.run();
+}
+
+}
---
+++ MynewMavenproject/src/main/java/pack/C.java
@@ -0,0 +1,17 @@
+package pack;
+
+public class C extends B{
+	int n=11;
+
+
+	public static void main(String[] args) {
+		C c=new C();
+		c.run();
+		c.test();
+		System.out.println(c.a);
+		System.out.println(c.college);
+		System.out.println(c.n);
+
+	}
+
+}
